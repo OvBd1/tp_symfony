ea7444687299a7f0df8ebc39efaa225aaa03b458
OvBd <y.bontrond@ecole-ipssi.net>
2026-02-06 12:51:28 +0100

    Page + modif Profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('', name: 'app_profile_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès !');
+
+            return $this->redirectToRoute('app_profile_show');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
+    }
+}
